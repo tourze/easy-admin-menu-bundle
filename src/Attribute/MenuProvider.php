0186dba7adcd7d012346befe8a5202ac09e1cc5e
github-ci <you@example.com>
2025-04-22 05:22:20 +0000

    fix: 修正菜单提供者标签名称错误

--- src/Attribute/MenuProvider.php
+++ src/Attribute/MenuProvider.php
@@ -7,8 +7,10 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 #[\Attribute(\Attribute::TARGET_CLASS)]
 class MenuProvider extends AutoconfigureTag
 {
+    const TAG_NAME = 'easy-admin-menu.provider';
+
     public function __construct()
     {
-        parent::__construct('app.admin.menu');
+        parent::__construct(self::TAG_NAME);
     }
 }
